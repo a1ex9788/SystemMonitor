ad1b263af1623ea40aeacbb8f8014057618153d8
a1ex9788 <alejandrocarrion1999@gmail.com>
2024-03-18 16:23:59 +0100

    Feat: Permitir invocar la herramienta con una abreviatura.

---
+++ Code/SystemMonitor/Tests/IntegrationTests/ToolTests.cs
@@ -0,0 +1,29 @@
+using CliWrap;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemMonitor.Tests.IntegrationTests
+{
+    [TestClass]
+    public class ToolTests
+    {
+        [TestMethod]
+        public async Task Tool_AbbreviatedName_ToolExecutedCorrectly()
+        {
+            // Arrange.
+            StringBuilder stringBuilder = new StringBuilder();
+
+            Command command = Cli.Wrap("sm")
+                .WithArguments("--help")
+                .WithStandardOutputPipe(PipeTarget.ToStringBuilder(stringBuilder));
+
+            // Act.
+            await command.ExecuteAsync();
+
+            // Assert.
+            stringBuilder.ToString().Should().Contain("Usage: systemMonitor [options]");
+        }
+    }
+}
---
+++ Code/SystemMonitor/sm.bat
@@ -0,0 +1,2 @@
+@echo off
+SystemMonitor.exe %*
