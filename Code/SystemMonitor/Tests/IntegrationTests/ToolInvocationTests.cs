ec56c74a69f0107dd8d4e5ec9f3ce1458ea33291
a1ex9788 <alejandrocarrion1999@gmail.com>
2024-03-18 17:03:21 +0100

    Refactor: Renombrar clase de test de invocación de la herramienta.

--- Code/SystemMonitor/Tests/IntegrationTests/ToolInvocationTests.cs
+++ Code/SystemMonitor/Tests/IntegrationTests/ToolInvocationTests.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace SystemMonitor.Tests.IntegrationTests
 {
     [TestClass]
-    public class ToolTests
+    public class ToolInvocationTests
     {
         [TestMethod]
         public async Task Tool_AbbreviatedName_ToolExecutedCorrectly()
